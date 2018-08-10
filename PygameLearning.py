a015f563ed5aaae96d44b560d2eb812048e8b708
zhanhaor <31212257+zhanhaor@users.noreply.github.com>
2018-08-10 14:11:23 -0700

    Add files via upload

---
+++ PygameLearning.py
@@ -0,0 +1,55 @@
+import pygame, sys
+
+Width = 800
+Height = 500
+Black = (255, 255, 255)
+White = (  0,   0,   0)
+
+def runGame():
+	pygame.init()
+	size = Width, Height
+	speed = [1, 1]
+	BLACK = (0, 0, 0)
+	screen = pygame.display.set_mode(size)
+	pygame.display.set_caption("Pygame壁球")
+	ball = pygame.image.load("PYG02-ball.gif")
+	ballrect = ball.get_rect()
+	fps = 30000
+	fclock = pygame.time.Clock()
+
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					speed[0] = speed[0] if speed[0] == 0 else(abs(speed[0]) - 1)*int(speed[0]/abs(speed[0]))
+				elif event.key == pygame.K_RIGHT:
+					speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0] - 1
+				elif event.key == pygame.K_UP:
+					speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1] - 1
+				elif event.key == pygame.K_DOWN:
+					speed[1] = speed[1] if speed[1] == 0 else(abs(speed[1]) - 1)*int(speed[1]/abs(speed[1]))
+				elif event.key == pygame.K_ESCAPE:
+					sys.exit()
+
+
+		ballrect = ballrect.move(speed[0], speed[1])
+		if ballrect.left < 0 or ballrect.right > Width:
+			speed[0] = -speed[0]
+		if ballrect.top  < 0 or ballrect.bottom > Height:
+			speed[1] = -speed[1]
+
+		screen.fill(BLACK)
+		screen.blit(ball, ballrect)
+		fclock.tick(fps)
+		pygame.display.update()
+
+
+if __name__ == '__main__':
+	runGame()
+
+
+
+	
